9ab2a54162a627250d702c8538889e6bebbc3027
RAHULKASH <rahulkashyap2108@gmail.com>
2026-01-21 16:45:16 +0530

    server adjustments

--- Connection/server.js
+++ Connection/server.js
@@ -12,9 +12,12 @@ connectDB();
 const app=express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+    origin: [
+      "http://localhost:5173",        
+      "https://taskmate-r4.netlify.app" 
+    ],
+    credentials: true
+  }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth",authRoutes);
